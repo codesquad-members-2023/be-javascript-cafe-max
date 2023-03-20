d973358b0795c54c474568f58a6b55470b7d601f
CDchan <kdjin1215@Naver.com>
2023-03-20 19:43:20 +0900

    feat: setPlaceholder.js구현
    register2에서 placeholder에 register1에서 입력했던 이메일과 nickName이 유지되도록 한다.

---
+++ js/setPlaceholder.js
@@ -0,0 +1,11 @@
+const urlParams = new URLSearchParams(window.location.search);
+const register1email = urlParams.get("email");
+const register1nickName = urlParams.get("nickName");
+
+const input = document.querySelector("input");
+input.placeholder = register1email;
+
+const email = document.getElementById("login-email");
+email.placeholder = register1email;
+const nickName = document.getElementById("login-nickName");
+nickName.placeholder = register1nickName;
