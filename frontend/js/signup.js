bcd0e5d04fe9af2d02cfd0d96f3c54cbf5288208
Jeongyong Park <1239824@naver.com>
2023-03-21 11:38:47 +0900

    feat : 회원가입 성공시 성공페이지의 모습을 띄워주도록 설정

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -1,24 +1,31 @@
-const signupForm = document.querySelector('#signup-form');
+let isSignupSuccess = false;
 
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+function onSignupBtnClick() {
+    const signupForm = document.querySelector('#signup-form');
 
-    if (!validateEmail()) {
-        alert('유효한 이메일 형식이 아닙니다.');
-        return;
-    }
-    if (!validateNickname()) {
-        alert('이름은 2글자 이상 64글자 이하여야 합니다.');
-        return;
-    }
-    if (!validatePassword()) {
-        alert('패스워드는 8글자 이상, 32글자 이하이며, 영어 소문자와 숫자를 반드시 포함해야 합니다.');
-        return;
-    }
-
-    alert('회원가입이 완료되었습니다.');
-    signupForm.submit();
-});
+    signupForm.addEventListener('submit', (e) => {
+        if (!isSignupSuccess) {
+            e.preventDefault();
+        
+            if (!validateEmail()) {
+                alert('유효한 이메일 형식이 아닙니다.');
+                return;
+            }
+            if (!validateNickname()) {
+                alert('이름은 2글자 이상 64글자 이하여야 합니다.');
+                return;
+            }
+            if (!validatePassword()) {
+                alert('패스워드는 8글자 이상, 32글자 이하이며, 영어 소문자와 숫자를 반드시 포함해야 합니다.');
+                return;
+            }
+        
+            alert('회원가입이 완료되었습니다.');
+            isSignupSuccess = true;
+            onSignupSuccess();
+        }
+    });
+}
 
 function validateNickname() {
     const nickname = document.getElementById('nickname').value.trim();
@@ -36,3 +43,18 @@ function validatePassword() {
     const passwordRegex = /^(?=.*[a-z])(?=.*\d)[a-z\d]{8,32}$/;
     return passwordRegex.test(password);
 }
+
+function onSignupSuccess() {
+    document.getElementById('guide-message').textContent = '가입이 완료되었습니다!';
+    document.getElementById('nickname').disabled = true;
+    document.getElementById('email').disabled = true;
+    document.getElementById('password-form').style.display = 'none';
+
+    const signupBtn = document.getElementById('signupBtn');
+    signupBtn.textContent = '지금 로그인하기';
+
+    const signupForm = document.getElementById('signup-form');
+    signupForm.setAttribute("action", "/frontend/html/screens/login.html");
+}
+
+onSignupBtnClick();
