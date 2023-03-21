2314c4ad450aa0e247177e6f5d9fad15a4e845fb
sudago <sudago22@gmail.com>
2023-03-21 17:11:04 +0900

    feat: 입력값을 검증하는 기능 추가
    1. html 태그에서의 이벤트를 js로 분리.
    2. 이메일, 이름, 비밀번호에 대한 유효성을 검증하고, 유효성을 통과하면 다음 페이지로 이동하게 설정.

--- mini-mission/js/sign-in-success.js
+++ mini-mission/js/sign-in-success.js
@@ -1,11 +1,11 @@
 const btn = document.querySelector("button");
 const email = document.querySelector("input[name=email]");
-const password = document.querySelector("input[name=password]");
+const username = document.querySelector("input[name=username]");
 const form = document.querySelector("form");
 
-// navigation에 있는 button 클릭 시, 회원가입 페이지로 이동
+// navigation에 있는 button 클릭 시, 로그인 페이지로 이동
 btn.addEventListener("click", function(){
-    location.replace("sign-in.html");
+    location.replace("login.html");
 });
 
 // 유효한 이메일인지 확인.
@@ -14,20 +14,20 @@ function validate_email(){
     return reg.test(email.value);
 }
 
-// 유효한 패드워드인지 확인.
-function validate_password() {
-    const reg = /^(?=.*[a-z])(?=.*\d)[a-z\d]{8,32}$/;
-    return reg.test(password.value);
-};
+// 사용자 이름이 유효한지 확인
+function validate_username(){
+    const reg = /^\w{2,64}$/;
+    return reg.test(username.value);
+}
 
 // 둘 다 유효하면 submit으로 넘어가기.
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    if (validate_password() && validate_email()) {
+    if (validate_email() && validate_username()) {
         location.replace("main.html");
     } else {
         let info_message = validate_email() ? "" : "이메일 ";
-        info_message += validate_password() ? "" : "비밀번호 ";
+        info_message += validate_username() ? "" : "이름 ";
         document.getElementById("access-denied-text").innerHTML = info_message + "을/를 다시 입력해주세요";
     }
 });
--- mini-mission/js/sign-in.js
+++ mini-mission/js/sign-in.js
@@ -1,17 +1,24 @@
 const btn = document.querySelector("button");
 const email = document.querySelector("input[name=email]");
+const username = document.querySelector("input[name=username]");
 const password = document.querySelector("input[name=password]");
 const form = document.querySelector("form");
 
-// navigation에 있는 button 클릭 시, 회원가입 페이지로 이동
+// navigation에 있는 button 클릭 시, 로그인 페이지로 이동
 btn.addEventListener("click", function(){
-    location.replace("sign-in.html");
+    location.replace("login.html");
 });
 
 // 유효한 이메일인지 확인.
 function validate_email(){
     const reg = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     return reg.test(email.value);
+}
+
+// 사용자 이름이 유효한지 확인
+function validate_username(){
+    const reg = /^\w{2,64}$/;
+    return reg.test(username.value);
 }
 
 // 유효한 패드워드인지 확인.
@@ -23,10 +30,11 @@ function validate_password() {
 // 둘 다 유효하면 submit으로 넘어가기.
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    if (validate_password() && validate_email()) {
-        location.replace("main.html");
+    if (validate_email() && validate_username() && validate_password()) {
+        location.replace("sign-in-success.html");
     } else {
         let info_message = validate_email() ? "" : "이메일 ";
+        info_message += validate_username() ? "" : "이름 ";
         info_message += validate_password() ? "" : "비밀번호 ";
         document.getElementById("access-denied-text").innerHTML = info_message + "을/를 다시 입력해주세요";
     }
