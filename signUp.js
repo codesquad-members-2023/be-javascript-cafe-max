a1615e7a67d2f7b8ac8ab5f14d27c8e70278a30a
chung <chunghye1998>
2023-03-23 01:08:01 +0900

    fix: 회원가입 검증 로직 수정

--- signUp.js
+++ signUp.js
@@ -1,23 +1,22 @@
-function submit(){
-    const signupForm = document.getElementById("signupForm")
+const submit = document.getElementById("submit");
+submit.addEventListener("click", validate);
 
-    signupForm.addEventListener("submit", validate());
-    function validate() {
-        if(!validateEmail()) {
-            alert("이메일 형식이 알맞지 않습니다.");
-            return;
-        }
-        if(!validateNickname()) {
-            alert("닉네임은 2이상 64이하의 글자수를 가져야 합니다.");
-            return;
-        }
-        if(!validatePassword()) {
-            alert("패스워드는 8이상 32이하의 글자수를 가져야 하며, 영어 소문자 및 숫자를 반드시 포함해야 합니다.");
-            return;
-        }
-        alert("가입이 완료되었습니다.");
+function validate() {
+    if(!validateEmail()) {
+        alert("이메일 형식이 알맞지 않습니다.");
+        return;
     }
+    if(!validateNickname()) {
+        alert("닉네임은 2이상 64이하의 글자수를 가져야 합니다.");
+        return;
+    }
+    if(!validatePassword()) {
+        alert("패스워드는 8이상 32이하의 글자수를 가져야 하며, 영어 소문자 및 숫자를 반드시 포함해야 합니다.");
+        return;
+    }
+    alert("가입이 완료되었습니다.");
 }
+
 
 function validateEmail() {
     const email = document.getElementById("email").value;
