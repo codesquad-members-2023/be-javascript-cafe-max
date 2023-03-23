93a73ab0b406f6c7823aeb783c535778560ac804
crtEvent <crtevn@gmail.com>
2023-03-24 03:29:56 +0900

    add: 글자수 표시 기능 추가

--- src/js/write_form.js
+++ src/js/write_form.js
@@ -10,3 +10,17 @@ var quill = new Quill('#quillEditor', {
     placeholder: '여기에 게시글을 작성하세요!',
     theme: 'snow'
 });
+
+/**
+ * 에디터의 글자수 제한 표시
+ */
+const limit = 1000;
+const htmlTag = document.getElementById("maxLength");
+quill.on('text-change', function (delta, old, source) {
+    let textLength = quill.getLength() - 1;
+    htmlTag.style.color = "green";
+    htmlTag.innerHTML = "글자수: " + textLength + "/1000";
+    if (textLength > limit) {
+        htmlTag.style.color = "red";
+    }
+});
