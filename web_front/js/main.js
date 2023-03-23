70f444381c1f54f33de70262cb42e8a3fa68bcaf
DOEKYONG <apochmn@naver.com>
2023-03-24 03:25:13 +0900

    feat : 글 상세 페이지 이동 함수추가, json 파일로 분리 url 에 글 번호를 붙혀 글상세 페이지로 이동하게함

--- web_front/js/main.js
+++ web_front/js/main.js
@@ -1,24 +1,41 @@
 const loginInfo = localStorage.getItem("loginInfo");
 console.log(loginInfo);
-(function() {		
-    let jsonList = new Array() ;
-    for(let i=1; i<=7; i++){        
-        let data = new Object() ;
-        data.title = i+"번 게시물 입니다." ;
-        data.nickname = "유저" + i ;
-        data.date = "2023.12.25",
-        data.viewcount = i;
-        data.content = "글 내용글 내용글 내용글 내용글 내용\n"+
-        "글 내용글 내용글 내용글 내용글 내용\n"+
-        "글 내용글 내용글 내용글 내용글 내용\n"+
-        "글 내용글 내용글 내용글 내용글 내용\n"+
-        "글 내용글 내용글 내용글 내용글 내용\n"+
-        "글 내용글 내용글 내용글 내용글 내용\n"        
-        jsonList.push(data) ;
-    }
-    let jsonData = JSON.stringify(jsonList);    
-    getList(jsonList);
-})();
+let jsonList = [];
+let  jsonData ;
+if(localStorage.getItem("board")==null){
+    jsonList = loadBoardDataJson();
+    jsonData= JSON.stringify(jsonList);
+    localStorage.setItem("board",jsonData);
+}else{
+    jsonData = localStorage.getItem("board");
+    jsonList = JSON.parse(jsonData)
+}
+
+function loadBoardDataJson() {
+    return JSON.parse(JSON.stringify(boardData));
+}
+
+
+getList();
+
+// function makeJson() {
+//     let jsonList = new Array() ;
+//     for(let i=0; i<7; i++){        
+//         let data = new Object() ;
+//         data.title = i+"번 게시물 입니다." ;
+//         data.nickname = "유저" + i ;
+//         data.date = "2023.12.25",
+//         data.viewcount = i;
+//         data.content = "글";
+//         data.reply =[];        
+//         jsonList.push(data) ;
+//     }
+//     console.log(jsonList);
+//     let jsonData = JSON.stringify(jsonList);
+//     console.log(jsonData);
+//     localStorage.setItem("board",jsonData);    
+//     return jsonList;
+// }
 
 function getlogOut(){
    const name =  localStorage.getItem("loginInfo")
@@ -32,12 +49,13 @@ document.getElementById('logout').addEventListener('click',function(){
     location.href="main.html"
 });
 
-function getList(jsonList) {
+function getList() {
+
     getlogOut();
     let html = "";
     for(let i = 0; i<jsonList.length; i++) {
         html +=
-        '<tr class="td_line" id = line'+i+'>'+
+        '<tr class="td_line" id = line'+i+' onclick="movePage('+i+')">'+
         '<td class="title">'+jsonList[i]["title"]+'</td>'+
         '<td class="nickname">'+jsonList[i]["nickname"]+'</td>'+
         '<td class="date">'+jsonList[i]["date"]+'</td>'+
@@ -46,6 +64,8 @@ function getList(jsonList) {
     }
     document.getElementById('count_post').innerHTML = "전체 글 : "+jsonList.length;
     document.getElementById('list').innerHTML += html;
+}
 
-
+function movePage(i) {
+    location.href ="../board/boardinfo.html?num="+i;
 }
