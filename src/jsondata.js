294b7f069ad4c2b1c07a74e17f44abd26cd88a30
JIHYE <jdh324@naver.com>
2023-03-24 05:21:10 +0900

    json 데이터를 이용하여 메인 페이지 게시판 구현

---
+++ src/jsondata.js
@@ -0,0 +1,31 @@
+const board = JSON.parse(jsondata());
+createBoard();
+
+function jsondata() {
+    var board = new Array();
+		
+	for (var i = 0; i <= 50; i++) {
+		var data = new Object();
+			
+		data.title = "게시물 제목 #"+ i;
+		data.nickname = "닉네임" + i;
+        data.date = "2023.03." + (24 + i);
+        data.hits = Math.floor(Math.random() * 1000);
+
+		board.push(data) ;
+	}
+
+	return JSON.stringify(board) ;
+}
+
+function createBoard() {
+    var content = "";
+    for (var i = 0, j = 0; i < 8; i++, j++) {
+        content += "<div class='column'>";
+        content += "<div class='title'><a href='#'>" + board[i].title + "</a></div>";
+        content += "<div class='writer'>" + board[i].nickname + "</div>";
+        content += "<div class='date'>" + board[i].date + "</div>";
+        content += "<div class='hits'>" + board[i].hits + "</div></div>";
+    }
+    document.querySelector(".list").innerHTML += content;
+}
