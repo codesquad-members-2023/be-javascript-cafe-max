7f66e9cc1cb53dbbdef0a71da6737fd96250bf96
DOEKYONG <apochmn@naver.com>
2023-03-25 06:02:09 +0900

    feat : 게시글 등록 로컬스토리지를 가져온 후 글 입력 정보들을 추가해 주고 가공한 결과물을 다시 로컬스토리지로 등록해줌

--- web_front/js/boardwrite.js
+++ web_front/js/boardwrite.js
@@ -24,7 +24,33 @@ document.getElementById('write').addEventListener('click',function(e){
        e.preventDefault();
        return;
     }
+    let title = document.getElementById('title').value;
+    let content = document.getElementById('content').value;
+    let board = JSON.parse(localStorage.getItem("board"));
+    let name = localStorage.getItem("loginInfo").split("@")[0];
+    let data = {};
+    data["title"] = title;
+    data["nickname"] = name;
+    data["date"] = createDate();
+    data["viewcount"] = 0;
+    data["content"] = content;
+    data["reply"] =[];
+    board.push(data);
+    localStorage.setItem("board",JSON.stringify(board));
+    
+
     const form = document.getElementById("form");
     form.action = '../main/main.html';
     form.submit();  
 });
+
+function createDate(){
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+    var hours = ('0' + today.getHours()).slice(-2); 
+    var minutes = ('0' + today.getMinutes()).slice(-2);
+    var seconds = ('0' + today.getSeconds()).slice(-2);
+    return year + '-' + month  + '-' + day;
+}
