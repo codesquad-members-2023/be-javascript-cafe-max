d6b0537069cee0c823513461728a72ccc2e6ff5f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-24 17:41:24 +0900

    feat: Post, Posts 객체 추가
    * Posts 객체는 Post 객체들을 관리하는 클래스입니다.

--- resources/static/js/post.js
+++ resources/static/js/post.js
@@ -1,7 +1,8 @@
 // 게시글
 class Post {
-  constructor(title, author, date) {
+  constructor(title, content, author, date) {
     this.title = title;
+    this.content = content;
     this.author = author;
     this.date = date;
     this.views = 0;
