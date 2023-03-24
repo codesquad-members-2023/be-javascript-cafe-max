f40cf738d324fbe927a866fef3d2cb4c65623fd6
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-24 14:44:54 +0900

    feat: 로그인 여부에 따른 전용 버튼 보이게 하는 기능 추가

--- resources/static/js/index.js
+++ resources/static/js/index.js
@@ -2,11 +2,15 @@ import {Page} from "./page.js"
 import {fetchPost} from "./post_generator.js"
 import {buildBoard} from "./board.js";
 import {buildPage, parsingCurrentPage} from "./page.js";
+import {checkLogin} from "./header.js";
 
 window.onload = async function () {
   const posts = await fetchPost("/cafe/resources/static/json/post.json")
   const currentPage = await parsingCurrentPage()
   const page = new Page(currentPage, 5, 10, posts.length)
+
+  // 로그인 여부에 따른 헤더 출력
+  checkLogin()
 
   // 게시글 출력
   const tbody = document.querySelector("#board_table tbody")
@@ -18,7 +22,6 @@ window.onload = async function () {
   // 전체 글 개수 출력
   const countOfPost = document.querySelector("#countOfPost")
   countOfPost.textContent = posts.length
-
 
 }
 
