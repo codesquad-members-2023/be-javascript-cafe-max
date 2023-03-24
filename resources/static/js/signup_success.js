7b034aec3d948a5ef367b7b5471651d9d9bf91a3
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-24 17:44:15 +0900

    modify: 회원가입 성공시 Members 객체를 사용하도록 수정

--- resources/static/js/signup_success.js
+++ resources/static/js/signup_success.js
@@ -1,7 +1,9 @@
+import {members} from "./members.js";
+
 $(document).ready(function () {
   const email = localStorage.getItem("signup_success_email")
-  const memberInfos = JSON.parse(localStorage.getItem("memberInfos"))
-  const member = memberInfos.find((memberInfo) => memberInfo.email === email)
+  const member = members.findEmail(email)
+  console.log(member)
   $("#email").val(member.email)
   $("#nickname").val(member.nickname)
 
