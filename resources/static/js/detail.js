60fa6bc2942e5728ddfa67fa752c99548b560471
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-25 16:11:51 +0900

    feat: 세부내용 글보기 페이지에 id값에 따른 게시글 내용을 태그에 추가

---
+++ resources/static/js/detail.js
@@ -0,0 +1,34 @@
+import {createPosts, Posts} from "./posts.js";
+
+window.onload = async function () {
+  const id = validateParam()
+  const post = await findPostById(id)
+  outputPost(post)
+}
+
+function validateParam() {
+  const params = new URLSearchParams(location.search)
+  const id = params.get("id")
+  if (id == null) {
+    location.href = "/cafe/resources/index.html"
+  }
+  return id
+}
+
+async function findPostById(id) {
+  const datas = await createPosts()
+  const posts = new Posts(datas)
+  return posts.findById(id)[0]
+}
+
+function outputPost(post) {
+  $("#title")[0].textContent = post.title
+  $("#author")[0].textContent = post.author
+  $("#regdate")[0].textContent = toLocalDate(post.date)
+  $("#views")[0].textContent = post.views
+  $("#content")[0].textContent = post.content
+}
+
+function toLocalDate(date) {
+  return new Date(date).toLocaleDateString()
+}
