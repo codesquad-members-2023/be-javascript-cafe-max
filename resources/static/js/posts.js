63ace51cbcefb91456a15233bf9d273113249cd9
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-25 14:57:57 +0900

    feat: 로컬 스토리지에서 게시글 데이터를 가져오는 기능 추가
    * 로컬 스토리지에서 "posts"라는 이름의 게시글 데이터가 없으면 샘플 데이터에서 데이터를 가져와 posts 리스트에 저장하여 로컬 스토리지에 저장합니다.
    * 기존 "posts" 데이터가 로컬 스토리지에 있으면 파싱 및 정렬하여 반환합니다.

--- resources/static/js/posts.js
+++ resources/static/js/posts.js
@@ -1,3 +1,5 @@
+import {fetchPost} from "./post_generator.js";
+
 class Posts {
   #posts
 
@@ -7,13 +9,44 @@ class Posts {
   }
 
   add(post) {
-    this.#posts.push(JSON.stringify(post))
+    this.#posts.push(post.toJSON())
     localStorage.setItem("posts", JSON.stringify(this.#posts))
   }
 
   get posts() {
     return this._posts;
   }
+
+  length() {
+    return this._posts.length
+  }
+
+  slice(startIndex, endIndex) {
+    return this._posts.slice(startIndex, endIndex)
+  }
+
 }
 
+async function createPosts() {
+  let posts = localStorage.getItem("posts")
+  if (posts !== null) {
+    const parsePosts = JSON.parse(posts)
+    parsePosts.sort(descDate)
+    return parsePosts
+  }
+  const datas = await fetchPost("/cafe/resources/static/json/post.json")
+  posts = []
+  datas.forEach((item) => posts.push(item))
+  posts.sort(descDate)
+  localStorage.setItem("posts", JSON.stringify(posts))
+  return posts
+}
+
+function descDate(a, b) {
+  const date1 = new Date(a.date)
+  const date2 = new Date(b.date)
+  return date2 - date1
+}
+
+export {Posts, createPosts}
 
