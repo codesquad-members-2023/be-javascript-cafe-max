d950b295d122d1bbac0cb742a0666e76892d8d68
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-25 16:12:27 +0900

    modify: Post 객체 정보에 id 요소 추가

--- resources/static/js/post_generator.js
+++ resources/static/js/post_generator.js
@@ -4,12 +4,14 @@ import {Post} from "./post.js"
 function generate_post() {
   const result = [];
   for (let i = 0; i < 120; i++) {
+    let id = i + 1
     let title = "게시물 제목" + ((i % 15) + 1);
     let content = "내용" + ((i % 15) + 1)
-    let author = "사용자" + ((i % 15) + 1);
+    let author = "user" + ((i % 15) + 1);
     let date = new Date()
-    result.push(new Post(title, content, author, date));
+    result.push(new Post(id, title, content, author, date));
   }
+  console.log(JSON.stringify(result))
   return result;
 }
 
