c3e46a6ce88f272b7784f1c16eff52e23f56c4a6
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-03-25 14:52:55 +0900

    modify: 샘플 게시글 데이터 생성시 Date 객체를 저장하도록 변경
    * 변경한 이유는 작성일자에서 시간까지 저장 및 정렬하여 가장 최근에 작성된 게시글이 앞쪽으로 오게 하기 위해서입니다.

--- resources/static/js/post_generator.js
+++ resources/static/js/post_generator.js
@@ -3,12 +3,12 @@ import {Post} from "./post.js"
 // 샘플 json 데이터 생성하여 반환
 function generate_post() {
   const result = [];
-  const today = new Date();
   for (let i = 0; i < 120; i++) {
     let title = "게시물 제목" + ((i % 15) + 1);
+    let content = "내용" + ((i % 15) + 1)
     let author = "사용자" + ((i % 15) + 1);
-    let date = today.toLocaleDateString();
-    result.push(new Post(title, author, date));
+    let date = new Date()
+    result.push(new Post(title, content, author, date));
   }
   return result;
 }
@@ -21,4 +21,4 @@ function fetchPost(path) {
   })
 }
 
-export {fetchPost}
+export {generate_post, fetchPost}
